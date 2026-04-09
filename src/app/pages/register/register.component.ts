b5dccc88ce4e6353befd758dac118e461a986e1a
Rasul Adasov <rasul@futureblur.com>
2026-04-09 11:48:14 +0200

    feat: add register functionality

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-register',
@@ -19,6 +20,8 @@ export class RegisterComponent implements OnInit, OnDestroy {
   private prevHtmlOverflow = '';
   private prevBodyOverflow = '';
 
+  constructor(private supabase: SupabaseService) {}
+
   ngOnInit(): void {
     this.prevHtmlOverflow = document.documentElement.style.overflow;
     this.prevBodyOverflow = document.body.style.overflow;
@@ -33,6 +36,17 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   onSubmit(event: Event): void {
     event.preventDefault();
-    // Registrierungs-API folgt
+    this.onRegister();
+  }
+
+  async onRegister() {
+    try {
+      await this.supabase.signUp(
+        "test@test.com", this.password,
+        this.username, this.age!, this.heightCm!
+      );
+    } catch(error) {
+      console.error("Error:", error);
+    }
   }
 }
